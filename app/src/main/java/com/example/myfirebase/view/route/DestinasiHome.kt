728baaf7a671469f6040e61665e84747576aefd3
dhitozarfhan <dhitozarfhan@gmail.com>
2026-01-02 21:06:09 +0700

    memperbarui nama object Destinasi Home

--- app/src/main/java/com/example/myfirebase/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/myfirebase/view/route/DestinasiHome.kt
@@ -2,7 +2,7 @@ package com.example.myfirebase.view.route
 
 import com.example.myfirebase.R
 
-object DestinasiHome : DestinasiNavigasi {
+object Destinasi Home : DestinasiNavigasi {
     override val route = "home"
     override val titleRes = R.string.app_name
 }
