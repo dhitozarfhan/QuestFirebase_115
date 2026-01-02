5e8ddf948d169a8f9f07e3b388910cbf02563b9f
dhitozarfhan <dhitozarfhan@gmail.com>
2026-01-02 21:05:47 +0700

    memperbaiki penulisan nama interface DestinasiNavigasi

--- app/src/main/java/com/example/myfirebase/view/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/myfirebase/view/route/DestinasiEntry.kt
@@ -2,7 +2,7 @@ package com.example.myfirebase.view.route
 
 import com.example.myfirebase.R
 
-object DestinasiEntry : DestinasiNavigasi {
+object DestinasiEntry : Destinasi Navigasi {
     override val route = "item_entry"
     override val titleRes = R.string.entry_siswa
 }
