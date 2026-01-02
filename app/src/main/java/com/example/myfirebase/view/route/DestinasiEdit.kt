febee1c309b51059839f20f91e3af79c312bb501
dhitozarfhan <dhitozarfhan@gmail.com>
2026-01-02 21:05:20 +0700

    memperbaiki penulisan DestinasiNavigasi pada DestinasiEdit

--- app/src/main/java/com/example/myfirebase/view/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/myfirebase/view/route/DestinasiEdit.kt
@@ -2,7 +2,7 @@ package com.example.myfirebase.view.route
 
 import com.example.myfirebase.R
 
-object DestinasiEdit : DestinasiNavigasi {
+object DestinasiEdit : Destinasi Navigasi {
     override val route = "item_edit"
     override val titleRes = R.string.edit_siswa
     const val itemIdArg = "idSiswa"
